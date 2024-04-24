d63af5e5eee79fb76b9792f8bdf7b9dc35e8a769
Cole Smith <cole.f.smith1@gmail.com>
2024-04-24 09:35:46 -0400

    update example

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,40 @@
-import { StyleSheet, View } from "react-native";
+import { getSubjectAsync } from "expo-ios-bg-remover";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
 
 export default function App() {
-  return <View style={styles.container} />;
+  const [bgRemovedUri, setBgRemovedUri] = useState<string | null>(null);
+
+  const removeBg = async () => {
+    // load uri from local image
+    const localImage = require("./assets/car.jpg");
+    const asset = Image.resolveAssetSource(localImage);
+
+    const result = await getSubjectAsync(asset.uri);
+
+    setBgRemovedUri(result.uri);
+  };
+
+  useEffect(() => {
+    removeBg();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("./assets/car.jpg")}
+        style={{ width: 200, height: 200 }}
+        resizeMode="contain"
+      />
+      {bgRemovedUri && (
+        <Image
+          source={{ uri: bgRemovedUri }}
+          style={{ width: 200, height: 200 }}
+          resizeMode="contain"
+        />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
